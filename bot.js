8c7806b9ec7ca32157dd5f24266fbff58372a876
Miguel Barros <eomiguelbarros@gmail.com>
2023-01-13 19:10:15 -0300

    Config for add new commands

--- bot.js
+++ bot.js
@@ -1,13 +1,32 @@
 const Discord = require("discord.js");
-const client = new Discord.Client({ intents: ["MessageContent"] });
+const client = new Discord.Client({ intents: [1, 512, 32768, 2, 128] });
+const config = require("./config.json");
+const fs = require("fs");
 
 require("dotenv").config();
 
-client
-  .login(process.env.PRIVATE_BOT_TOKEN)
-  .then(() => {
-    console.log(`Aoba estou online no servidor (${client.user.username})🔥`);
-  })
-  .catch((err) => {
-    console.log(`Opps, tive um erro - ${err}`);
-  });
+client.login(process.env.PRIVATE_BOT_TOKEN);
+
+client.on("ready", () => {
+  console.log(`Aoba estou online no servidor (${client.user.username})🔥`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+  if (message.channel.type === Discord.ChannelType.DM) return;
+
+  if (message.content.startsWith(config.prefix)) {
+    const args = message.content
+      .slice(config.prefix.length)
+      .trim()
+      .split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    try {
+      const commandFile = require(`./commands/${command}.js`);
+      commandFile.run(client, message, args);
+    } catch (err) {
+      console.error("Erro:" + err);
+    }
+  }
+});
